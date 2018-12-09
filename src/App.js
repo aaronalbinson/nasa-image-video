ba0fc155293d2edc074bd4649ed2f8e3ca07c53d
Aaron <a.albinson@teluxhd.com>
2018-12-09 00:49:55 +0000

    Create SearchField component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import { withRouter } from "react-router";
+import SearchField from "../src/components/generic/SearchField";
 import theme from "./styles/theme";
 
 require("./App.css");
@@ -10,7 +11,7 @@ class App extends Component {
     return (
       <MuiThemeProvider theme={theme}>
         <div className="App">
-          <p>test</p>
+          <SearchField />
         </div>
       </MuiThemeProvider>
     );
