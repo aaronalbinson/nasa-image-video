355e31a185ba31e4122146e59a76df2d1b22336e
Aaron <a.albinson@teluxhd.com>
2018-12-09 15:18:26 +0000

    Add a search button and call api when pressed

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React, { Component } from "react";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import { withRouter } from "react-router";
-import search from "../src/api/search";
-import SearchField from "../src/components/generic/SearchField";
+import SearchForm from "../src/components/generic/SearchForm";
 import theme from "./styles/theme";
 
 require("./App.css");
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchQuery: "moon"
-    };
-  }
-
-  componentDidMount() {
-    this.loadSearchData();
-  }
-
-  loadSearchData() {
-    search(this.state.searchQuery);
-  }
-
   render() {
     return (
       <MuiThemeProvider theme={theme}>
         <div className="App">
-          <SearchField />
+          <SearchForm />
         </div>
       </MuiThemeProvider>
     );
