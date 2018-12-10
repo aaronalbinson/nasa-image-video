fc924ffdb2cc0a9655deea2c135a1ca2d0374a2e
Aaron <a.albinson@teluxhd.com>
2018-12-10 05:31:28 +0000

    Put asset within card component

--- src/components/generic/Asset/index.js
+++ src/components/generic/Asset/index.js
@@ -3,15 +3,19 @@ import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import getAsset from "../../../api/asset";
 import getAssetMeta from "../../../api/assetMeta";
+import "./asset.css";
 
 class Asset extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       assetImageData: "",
-      assetData: ""
+      assetData: "",
+      assetid: ""
     };
   }
   componentDidMount() {
@@ -20,6 +24,7 @@ class Asset extends React.Component {
 
   loadAssetData() {
     const assetid = this.props.location.pathname.split("/").pop();
+    this.setState({ assetid });
     getAsset(assetid).then(response => {
       const data = response.collection;
       this.setState({ assetImageData: data });
@@ -39,24 +44,32 @@ class Asset extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Grid container className="asset" spacing={24}>
-          <Grid item>
-            <Typography variant="h1">
-              {this.state.assetData["XMP:Title"]}
-            </Typography>
-          </Grid>
-          <Grid item>
-            <img
-              src={
-                this.state.assetImageData &&
-                this.state.assetImageData.items[0].href
-              }
-            />
-          </Grid>
-          <Grid item>
-            <Typography>{this.state.assetData["XMP:Description"]}</Typography>
-          </Grid>
-        </Grid>
+        <Card className="asset">
+          <CardContent>
+            <Grid container className="asset" spacing={24}>
+              <Grid item>
+                <Typography variant="h3">
+                  {this.state.assetData["XMP:Title"]
+                    ? this.state.assetData["XMP:Title"]
+                    : this.state.assetid}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <img
+                  src={
+                    this.state.assetImageData &&
+                    this.state.assetImageData.items[0].href
+                  }
+                />
+              </Grid>
+              <Grid item>
+                <Typography>
+                  {this.state.assetData["XMP:Description"]}
+                </Typography>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
       </React.Fragment>
     );
   }
